753fb052fcdee7fee493e2dc87bc4fba40a8280c
Ammar Abdo <90091361+AmmarAbdoh@users.noreply.github.com>
2023-06-22 11:09:06 +0300

    test commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from "./images/Daughter.jpg";
+import img2 from "./images/Dad.jpg";
+import img3 from "./images/Mom.jpg";
+export const books = [
+  {
+    author: "Gregory E. Lang",
+    title: "Why a Daughter Needs a Dad",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Gregory E. Lang",
+    title: "Why a Son Needs a Dad",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Gregory E. Lang",
+    title: "Why a Daughter Needs a Mom",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+import Book from "./book";
+import { books } from "./books";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+export default Book;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
